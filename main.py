90aef35133d247911eddca82461d8597fd11aa37
WingiM <wingimobile@gmail.com>
2020-12-16 16:14:18 +0300

    Template files and main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from Templates.Board import Board
+
+INDENT_LEFT, INDENT_TOP, CELL_SIZE = 10, 10, 150
+
+
+if __name__ == '__main__':
+    size = weight, height = 800, 800
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Мини-игры')
+    board = Board(3, 3, screen)
+    board.set_view(INDENT_LEFT, INDENT_TOP, CELL_SIZE)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 0, 0))
+        board.render()
+        pygame.display.flip()
+
